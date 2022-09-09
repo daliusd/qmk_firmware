f5d8500a768bad523dcc75d01d94af32f5098e4c
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2022-09-09 10:20:27 +0300

    a_dux layout with tap-holds

---
+++ keyboards/a_dux/keymaps/daliusdth/keymap.c
@@ -0,0 +1,221 @@
+/* Copyright 2021 @daliusd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+enum layers {
+    _QWERTY,
+    _SYM,
+    _NAV,
+    _MISC,
+    _TMUX,
+    _MOUSE,
+    _FUNC,
+    _LT,
+};
+
+enum custom_keycodes {
+  TM_NEXT = SAFE_RANGE,
+  TM_PREV,
+  TM_LEFT,
+  TM_RIGHT,
+  TM_NEW,
+  TM_SLCT,
+  TM_SRCH,
+  TM_URL,
+};
+
+// Shortcut to make keymap more readable
+
+#define L_NAV       MO(_NAV)
+#define L_SYM       MO(_SYM)
+#define L_MS_M      LT(_MOUSE, KC_M)
+#define L_TMUX_B    LT(_TMUX, KC_B)
+#define L_FUNC_N    LT(_FUNC, KC_N)
+#define L_MISC_D    LT(_MISC, KC_DOT)
+#define L_LT_COM    LT(_LT, KC_COMM)
+
+#define K_GUI_S     GUI_T(KC_S)
+#define K_CTL_D     CTL_T(KC_D)
+#define K_ALT_F     ALT_T(KC_F)
+
+#define K_ALT_J     RALT_T(KC_J)
+#define K_CTL_K     RCTL_T(KC_K)
+#define K_GUI_L     RGUI_T(KC_L)
+
+#define K_PRINT     (QK_LCTL | QK_LSFT | QK_LGUI | KC_4)
+#define K_VIDEO     (QK_LSFT | QK_LGUI | KC_5)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [_QWERTY] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┐                         ┌────────┬────────┬────────┬────────┬────────┐
+     KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T,                              KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,
+  //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
+     KC_A    ,K_GUI_S ,K_CTL_D ,K_ALT_F ,KC_G    ,                          KC_H    ,K_ALT_J ,K_CTL_K ,K_GUI_L ,KC_SCLN ,
+  //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
+     KC_Z    ,KC_X    ,KC_C    ,KC_V    ,L_TMUX_B,                          L_FUNC_N,L_MS_M  ,L_LT_COM,L_MISC_D,KC_SLSH,
+  //└────────┴────────┴────────┴────┬───┴────┬───┼────────┐       ┌────────┼───┬────┴───┬────┴────────┴────────┴────────┘
+                                     L_NAV   ,    KC_SPC  ,        KC_LSFT ,    L_SYM
+  //                                └────────┘   └────────┘       └────────┘   └────────┘
+  ),
+
+  [_SYM] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┐                         ┌────────┬────────┬────────┬────────┬────────┐
+     KC_EXLM ,KC_AT   ,KC_HASH ,KC_DLR  ,KC_PERC ,                          KC_CIRC ,KC_AMPR ,KC_ASTR ,KC_LPRN ,KC_RPRN ,
+  //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,KC_GRV  ,KC_LBRC ,KC_RBRC ,KC_PLUS ,                          KC_MINS ,KC_PIPE ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,XXXXXXX ,KC_LCBR ,KC_RCBR ,KC_EQL  ,                          KC_UNDS ,KC_QUOT ,KC_DQT  ,XXXXXXX ,KC_BSLS ,
+  //└────────┴────────┴────────┴────┬───┴────┬───┼────────┐       ┌────────┼───┬────┴───┬────┴────────┴────────┴────────┘
+                                     _______ ,    _______ ,        _______ ,    XXXXXXX
+  //                                └────────┘   └────────┘       └────────┘   └────────┘
+  ),
+
+  [_NAV] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┐                         ┌────────┬────────┬────────┬────────┬────────┐
+     KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                          KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,
+  //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
+     KC_TAB  ,KC_LGUI ,KC_LCTL ,KC_LALT, KC_ENT  ,                          KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RIGHT,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
+     KC_LSFT ,KC_BSPC ,KC_ESC  ,KC_TILDE,XXXXXXX ,                          XXXXXXX ,KC_DEL  ,KC_COMM ,KC_DOT  ,XXXXXXX ,
+  //└────────┴────────┴────────┴────┬───┴────┬───┼────────┐       ┌────────┼───┬────┴───┬────┴────────┴────────┴────────┘
+                                     XXXXXXX ,    _______ ,        _______ ,    _______
+  //                                └────────┘   └────────┘       └────────┘   └────────┘
+  ),
+
+  [_MISC] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┐                         ┌────────┬────────┬────────┬────────┬────────┐
+     RESET   ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,                          KC_BRID ,KC_BRIU ,KC_PSCR, XXXXXXX ,K_PRINT ,
+  //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,XXXXXXX ,DEBUG   ,XXXXXXX ,XXXXXXX ,                          KC_MPRV ,KC_MPLY ,KC_MNXT ,XXXXXXX, K_VIDEO ,
+  //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,                          KC_VOLD ,KC_VOLU ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+  //└────────┴────────┴────────┴────┬───┴────┬───┼────────┐       ┌────────┼───┬────┴───┬────┴────────┴────────┴────────┘
+                                     XXXXXXX ,    XXXXXXX ,        XXXXXXX ,    XXXXXXX
+  //                                └────────┘   └────────┘       └────────┘   └────────┘
+  ),
+
+  [_TMUX] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┐                         ┌────────┬────────┬────────┬────────┬────────┐
+     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,                          XXXXXXX ,TM_URL  ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,                          TM_LEFT ,TM_NEXT ,TM_PREV ,TM_RIGHT,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,                          TM_NEW  ,TM_SLCT ,XXXXXXX ,XXXXXXX ,TM_SRCH ,
+  //└────────┴────────┴────────┴────┬───┴────┬───┼────────┐       ┌────────┼───┬────┴───┬────┴────────┴────────┴────────┘
+                                     XXXXXXX ,    XXXXXXX ,        XXXXXXX ,    XXXXXXX
+  //                                └────────┘   └────────┘       └────────┘   └────────┘
+  ),
+
+  [_MOUSE] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┐                         ┌────────┬────────┬────────┬────────┬────────┐
+     XXXXXXX ,XXXXXXX ,KC_MS_U ,KC_BTN3 ,KC_WH_U ,                          XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,KC_MS_L ,KC_MS_D ,KC_MS_R ,KC_WH_D ,                          XXXXXXX ,KC_LALT ,KC_RCTL ,KC_RGUI ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,                          XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+  //└────────┴────────┴────────┴────┬───┴────┬───┼────────┐       ┌────────┼───┬────┴───┬────┴────────┴────────┴────────┘
+                                     KC_BTN1 ,    KC_BTN2 ,        _______ ,    XXXXXXX
+  //                                └────────┘   └────────┘       └────────┘   └────────┘
+  ),
+
+  [_FUNC] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┐                         ┌────────┬────────┬────────┬────────┬────────┐
+     KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,                          XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
+     KC_F6   ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,                          XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
+     KC_F11  ,KC_F12  ,XXXXXXX ,XXXXXXX ,XXXXXXX ,                          XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+  //└────────┴────────┴────────┴────┬───┴────┬───┼────────┐       ┌────────┼───┬────┴───┬────┴────────┴────────┴────────┘
+                                     XXXXXXX ,    XXXXXXX ,        XXXXXXX ,    XXXXXXX
+  //                                └────────┘   └────────┘       └────────┘   └────────┘
+  ),
+
+  [_LT] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┐                         ┌────────┬────────┬────────┬────────┬────────┐
+     KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                          XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
+     KC_6    ,KC_7    ,KC_8    ,KC_EQL  ,XXXXXXX ,                          XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+  //├────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┤
+     XXXXXXX ,KC_BSPC ,XXXXXXX ,XXXXXXX ,XXXXXXX ,                          XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,
+  //└────────┴────────┴────────┴────┬───┴────┬───┼────────┐       ┌────────┼───┬────┴───┬────┴────────┴────────┴────────┘
+                                     XXXXXXX ,    XXXXXXX ,        _______ ,    XXXXXXX
+  //                                └────────┘   └────────┘       └────────┘   └────────┘
+  ),
+};
+
+#define TMUX_PREFIX SS_DOWN(X_LCTL) "b" SS_UP(X_LCTL)
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case TM_LEFT:
+            if (!record->event.pressed) return true;
+            SEND_STRING(TMUX_PREFIX "<");
+            return false;
+        case TM_RIGHT:
+            if (!record->event.pressed) return true;
+            SEND_STRING(TMUX_PREFIX ">");
+            return false;
+        case TM_NEXT:
+            if (!record->event.pressed) return true;
+            SEND_STRING(TMUX_PREFIX "n");
+            return false;
+        case TM_PREV:
+            if (!record->event.pressed) return true;
+            SEND_STRING(TMUX_PREFIX "p");
+            return false;
+        case TM_NEW:
+            if (!record->event.pressed) return true;
+            SEND_STRING(TMUX_PREFIX "c");
+            return false;
+        case TM_SLCT:
+            if (!record->event.pressed) return true;
+            SEND_STRING(TMUX_PREFIX "[");
+            return false;
+        case TM_SRCH:
+            if (!record->event.pressed) return true;
+            SEND_STRING(TMUX_PREFIX "\t");
+            return false;
+        case TM_URL:
+            if (!record->event.pressed) return true;
+            SEND_STRING(TMUX_PREFIX SS_LCTL("u"));
+            return false;
+    }
+    return true;
+}
+
+bool lang_layer_on = false;
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    uint8_t hl = get_highest_layer(state);
+    if (hl == _LT) {
+        if (!lang_layer_on) {
+            tap_code16(LCTL(KC_SPC));
+            lang_layer_on = true;
+        }
+    } else {
+        if (lang_layer_on) {
+            tap_code16(LCTL(KC_SPC));
+            lang_layer_on = false;
+        }
+    }
+
+    return state;
+}
---
+++ keyboards/a_dux/keymaps/daliusdth/readme.md
@@ -0,0 +1,15 @@
+34 keys layout
+==============
+
+This is attempt to use tap-hold layout for 34 keys that is similar
+to the one I have with Callum layout
+(https://github.com/qmk/qmk_firmware/pull/16174). There are some
+changes that make layers more accessible, tab key moved to thumb
+key for faster access and some keys are moved as one shot is not
+available with this layout.
+
+Overall I like this more than Callum layout. Many shortcuts are
+more sensible as they do not require one extra key (as with
+Callum). I needed some time to get used to some differences and
+tap-hold keys but move from Callum to this layout took me about
+one hour.
