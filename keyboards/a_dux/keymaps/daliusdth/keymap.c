cc1e7fb17039a00007e8c1accdad1814dd26bf19
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2023-08-16 12:14:11 +0300

    Disable QUICK_TAP_TERM for NAV_TAB.

--- keyboards/a_dux/keymaps/daliusdth/keymap.c
+++ keyboards/a_dux/keymaps/daliusdth/keymap.c
@@ -258,3 +258,12 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
     return state;
 }
+
+uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case NAV_TAB:
+            return 0;
+        default:
+            return QUICK_TAP_TERM;
+    }
+}
