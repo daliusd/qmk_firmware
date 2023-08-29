db86dce5a20633136688c3c57bb327be4ca9e02b
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2023-08-30 00:58:47 +0300

    Accelerated with max speed max acceleration.

--- drivers/sensors/analog_joystick_barrett.c
+++ drivers/sensors/analog_joystick_barrett.c
@@ -26,7 +26,8 @@ uint16_t minAxisValue = ANALOG_JOYSTICK_AXIS_MIN;
 uint16_t maxAxisValue = ANALOG_JOYSTICK_AXIS_MAX;
 
 uint8_t maxCursorSpeed = ANALOG_JOYSTICK_SPEED_MAX;
-uint8_t speedRegulator = 40; // ANALOG_JOYSTICK_SPEED_REGULATOR; // Lower Values Create Faster Movement
+int16_t speedRegulator = 20; // ANALOG_JOYSTICK_SPEED_REGULATOR; // Lower Values Create Faster Movement
+int16_t stepsToMax = 20;
 
 int16_t xOrigin, yOrigin;
 
@@ -34,8 +35,17 @@ uint16_t lastCursor = 0;
 
 int16_t axisCoordinate(pin_t pin, uint16_t origin) {
     int16_t position = analogReadPin(pin);
-    return position - origin;
+    int16_t diff = position - origin;
+    int16_t range = diff < 0 ? (origin - minAxisValue) : (maxAxisValue - origin);
+
+    float   fraction    = (float)diff / range;
+    int16_t coordinate = (int16_t)(fraction * 100);
+
+    return coordinate > 100 ? 100 : coordinate < -100 ? -100 : coordinate;
 }
+
+float xC = 0;
+float yC = 0;
 
 report_analog_joystick_t analog_joystick_barrett_read(void) {
     report_analog_joystick_t report = {0};
@@ -44,13 +54,30 @@ report_analog_joystick_t analog_joystick_barrett_read(void) {
         lastCursor = timer_read();
 
         int16_t x = axisCoordinate(ANALOG_JOYSTICK_X_AXIS_PIN, xOrigin);
+        if (abs(x) < 5) {
+            xC = 0;
+        } else {
+            float maxX = (float)x / 100 * abs(x) * maxCursorSpeed / speedRegulator;
+            if (abs(xC) < abs(maxX)) {
+                xC += maxX / stepsToMax;
+            } else {
+                xC = maxX;
+            }
+        }
         int16_t y = axisCoordinate(ANALOG_JOYSTICK_Y_AXIS_PIN, yOrigin);
+        if (abs(y) < 5) {
+            yC = 0;
+        } else {
+            float maxY = (float)y / 100 * abs(y) * maxCursorSpeed / speedRegulator;
+            if (abs(yC) < abs(maxY)) {
+                yC += maxY / stepsToMax;
+            } else {
+                yC = maxY;
+            }
+        }
 
-        int8_t xCalc = x * maxCursorSpeed / speedRegulator;
-        int8_t yCalc = y * maxCursorSpeed / speedRegulator;
-
-        report.x   = xCalc;
-        report.y   = yCalc;
+        report.x   = (int8_t)xC;
+        report.y   = (int8_t)yC;
     }
 #ifdef ANALOG_JOYSTICK_CLICK_PIN
     report.button = !readPin(ANALOG_JOYSTICK_CLICK_PIN);
